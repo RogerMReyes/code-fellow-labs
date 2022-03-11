2e61b80a9b8a1322d11b8f6dc54a38562f0e6c89
Roger Reyes <rogerrmr14@gmail.com>
2022-03-11 15:29:29 -0800

    Cleaned up my script

--- js/app.js
+++ js/app.js
@@ -27,17 +27,14 @@ function firstFiveQuestions() {
       alert(yesResponseArray[i]);
       if (i !== 2) {
         totalCorrect++;
-        // console.log(totalCorrect);
       }
     } else {
       alert(noResponseArray[i]);
       if (i === 2) {
         totalCorrect++;
-        // console.log(totalCorrect);
       }
     }
   }
-  // console.log(totalCorrect);
 }
 firstFiveQuestions();
 
@@ -62,7 +59,6 @@ function guessingFingersGame() {
   } else {
     alert(`You're out of attempts ${userName} and still haven't gotten the right answer. I'm holding up 7 fingers!`);
   }
-  // console.log(totalCorrect);
 }
 guessingFingersGame();
 
@@ -94,55 +90,3 @@ function guessingFruit() {
   console.log(totalCorrect);
 }
 guessingFruit();
-
-
-// Old way of asking the first five Questions
-// let answerOne = prompt(`Hey ${userName}, Question time do you think I like video games?`).toLowerCase();
-// if (answerOne === 'y' || answerOne === 'yes') {
-//   alert('You\'re right! I am currently playing Elden Ring!');
-//   totalCorrect++;
-//   // console.log(`${userName} is correct I like video games`);
-// } else if (answerOne === 'n' || answerOne === 'no') {
-//   alert('Nope, I\'ve been gaming since I could first hold a gameboy');
-//   // console.log(`${userName} is wrong I do enjoy video games`);
-// }
-
-// let answerTwo = prompt('What about movies, do you think I love movies?').toLowerCase();
-// if (answerTwo === 'y' || answerTwo === 'yes') {
-//   alert('Right again I love watching movies especially in movie theaters');
-//   totalCorrect++;
-//   // console.log(`${userName} is right I do love movies`);
-// } else if (answerTwo === 'n' || answerTwo === 'no') {
-//   alert('Wrong, I love movies!');
-//   // console.log(`${userName} is wrong I do love movies`);
-// }
-
-// let answerThree = prompt('I\'m a movie lover so does that mean I like movie theater food?').toLowerCase();
-// if (answerThree === 'y' || answerThree === 'yes') {
-//   alert('Nope buying theater food isn\'t for me at the prices they\'re pushing.');
-//   // console.log(`${userName} was wrong I don't like paying for movie theater food`);
-// } else if (answerThree === 'n' || answerThree === 'no') {
-//   alert('You\'re right! I just try to bring my own snacks but don\'t tell anyone.');
-//   totalCorrect++;
-//   // console.log(`${userName} was right I don't like paying for movie theater food`);
-// }
-
-// let answerFour = prompt('When I am at home do you think my favorite streaming service is Netflix?').toLowerCase();
-// if (answerFour === 'y' || answerFour === 'yes') {
-//   alert('Yes! They\'ve got a little bit of everything on there.');
-//   totalCorrect++;
-//   // console.log(`${userName} was right I do like Netflix`);
-// } else if (answerFour === 'n' || answerFour === 'no') {
-//   alert('Sorry Netlix is my favorite!');
-//   // console.log(`${userName} was wrong Netflix is my favorite streaming service`);
-// }
-
-// let answerFive = prompt('Do you think I can binge watch an entire series?').toLowerCase();
-// if (answerFive === 'y' || answerFive === 'yes') {
-//   alert(`Yea it's probably not the healthiest thing for my body but sometimes it just happens. Well that's all the questions I have ${userName} talk to you again soon!`);
-//   totalCorrect++;
-//   // console.log(`${userName} has binged watched a series before`);
-// } else if (answerFive === 'n' || answerFive === 'no') {
-//   alert(`You underestimate my power ${userName} series are no match for me! That's all my questions for now ${userName}! Talk to you again soon!`);
-//   // console.log(`${userName} hasn't bing watched a series before`);
-// }
